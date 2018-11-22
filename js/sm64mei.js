2102431a9bc712caeb597e8ebbc7383fe7510603
Lewis Paterson <43826499+LewisPaterson97@users.noreply.github.com>
2018-11-22 19:46:09 +0000

    Added SMB and SM64 MEIs

--- js/sm64mei.js
+++ js/sm64mei.js
@@ -20,4 +20,4 @@ options = {
 vrvToolkit.setOptions(options)
 
 /* Show the score */
-showScore('score', 'data/Super_Mario_World_Overworld_Theme.mei') // <-- CHANGE THIS TO YOUR FILE
+showScore('score', 'data/Super_Mario_64_Main_Theme.mei') // <-- CHANGE THIS TO YOUR FILE
